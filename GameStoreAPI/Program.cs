431233c634914562015f1b993efd01de0c07e45a
Alexander Wilson <apwilson97@gmail.com>
2024-05-21 23:50:24 +0100

    Added connection string to appsettings.json and configuration to connect to Sqlite database using GameStoreContext

--- GameStoreAPI/Program.cs
+++ GameStoreAPI/Program.cs
@@ -4,7 +4,8 @@ using SQLitePCL;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = "Data Source=GameStore.db";
+// Configuration object implements IConfiguration and collects configuration information from all sources that has been configured for this application, including appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("GameStore");
 // Registering services for the builder to use using dependency injection, this creates connection to database
 // EFC is going to read connection string, create instance of GameStoreContext and pass in DbContextOptions that are going to contain all details
 // in connection string to connect to database
